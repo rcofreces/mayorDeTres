b80d519ca68ec88204fca59d0e2b3acd63356cc0
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 14:41:00 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+let número1: number = Number(prompt("Ingrese el número 1"));
+let número2: number = Number(prompt("Ingrese el número 2"));
+let número3: number = Number(prompt("Ingrese el número 3"));
+
+if (número1 > número2 && número3) {
+  console.log("El número mayor es:", número1);
+} else {
+  if (número2 > número1 && número3) {
+    console.log("El número mayor es:", número2);
+  } else {
+    if (número3 > número1 && número2);
+    console.log("El número mayor es:", número3);
+  }
+}
